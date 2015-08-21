f578f6577b173baa99b7ac63e407f6a587270bce
Idan Ratzabi <idanr@any.do>
2015-08-21 18:38:29 +0300

    Adding Facebook login

--- AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/LoginActivity.java
+++ AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/LoginActivity.java
@@ -1,9 +1,70 @@
 package com.example.idanr.tinderforjavaclass;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 /**
  * Created by idanr on 8/18/15.
  */
 public class LoginActivity extends Activity {
+
+    CallbackManager mCallbackManager;
+    private LoginButton mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login_activity);
+
+        mCallbackManager = CallbackManager.Factory.create();
+
+        mLoginButton = (LoginButton) findViewById(R.id.login_button);
+        mLoginButton.setReadPermissions("user_friends");
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
+    }
 }
+
--- AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/MainActivity.java
+++ AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.idanr.tinderforjavaclass;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent startLogin = new Intent(this, LoginActivity.class);
+        this.startActivity(startLogin);
     }
 
     @Override
