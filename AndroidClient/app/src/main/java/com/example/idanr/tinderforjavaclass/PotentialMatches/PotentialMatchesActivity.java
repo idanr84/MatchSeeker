727364418841e5604b762f0d0f33f49d10b556b9
Idan Ratzabi <idanr@any.do>
2015-10-23 18:15:36 +0300

    Adding like/dislike button

--- AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/PotentialMatches/PotentialMatchesActivity.java
+++ AndroidClient/app/src/main/java/com/example/idanr/tinderforjavaclass/PotentialMatches/PotentialMatchesActivity.java
@@ -2,14 +2,13 @@ package com.example.idanr.tinderforjavaclass.PotentialMatches;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.idanr.tinderforjavaclass.CustomUI.CardLayout.cardstack.CardStack;
+import com.example.idanr.tinderforjavaclass.CustomUI.CardLayout.cardstack.CardUtils;
 import com.example.idanr.tinderforjavaclass.Model.User;
 import com.example.idanr.tinderforjavaclass.R;
-
-import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -21,7 +20,15 @@ import butterknife.ButterKnife;
  */
 public class PotentialMatchesActivity extends Activity {
 
-    @Bind(R.id.cardLayout) CardStack cardLayout;
+    @Bind(R.id.cardLayout)
+    CardStack mCardLayout;
+
+    @Bind(R.id.likeButton)
+    ImageButton mLikeButton;
+
+    @Bind(R.id.dislikeButton)
+    ImageButton mDislikeButton;
+
     PotentialMatchesDataAdapter mAdapter;
 
     @Override
@@ -34,16 +41,30 @@ public class PotentialMatchesActivity extends Activity {
         ButterKnife.bind(this);
 
         //add the view via xml or programmatically
-        cardLayout.setStackMargin(20);
+        mCardLayout.setStackMargin(20);
 
         ArrayList<User> al = new ArrayList<User>();
         al.add(new User("idan","31"));
         al.add(new User("idan_1","31"));
         al.add(new User("idan_2","31"));
-        al.add(new User("idan_3","31"));
+        al.add(new User("idan_3", "31"));
 
         mAdapter = new PotentialMatchesDataAdapter(this,al);
 
-        cardLayout.setAdapter(mAdapter);
+        mCardLayout.setAdapter(mAdapter);
+
+        mLikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCardLayout.discardTop(CardUtils.DIRECTION_TOP_RIGHT,500);
+            }
+        });
+
+        mDislikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCardLayout.discardTop(CardUtils.DIRECTION_TOP_LEFT,500);
+            }
+        });
     }
 }
